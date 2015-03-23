c0f7ddc8d31523c66c793a7b0578284dffd272a3
Denis Makogon <dmakogon@mirantis.com>
2015-03-23 15:13:07 +0200

    Initial commit

---
+++ mcloudv_web/panels/views.py
@@ -0,0 +1,30 @@
+#    Copyright 2015 Mirantis, Inc
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from django.utils.translation import ugettext_lazy as _
+from horizon import tables
+
+from mcloudv_web.external_api import cloudvalidation_ostf_adapter
+from mcloudv_web.tables import ostf_table
+
+
+class IndexView(tables.DataTableView):
+    table_class = ostf_table.OSTFTable
+    template_name = 'mcloudv_web/panels/index.html'
+    page_title = _("Tests")
+
+    def get_data(self):
+        tests = (cloudvalidation_ostf_adapter.cloudvalidation_ostf_client().
+                 suites.list_tests_for_suites('fuel_health'))
+        return tests
---
+++ mcloudv_web/tables/ostf_table.py
@@ -0,0 +1,55 @@
+#    Copyright 2015 Mirantis, Inc
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import ungettext_lazy
+
+from horizon import tables
+
+from mcloudv_web.external_api import cloudvalidation_ostf_adapter
+
+
+class ExecuteTest(tables.BatchAction):
+    name = "execute"
+    classes = ('btn-confirm',)
+    help_text = _("Execute single test.")
+
+    @staticmethod
+    def action_present():
+        return ungettext_lazy(
+            u"Execute test",
+            u"Execute tests"
+        )
+
+    @staticmethod
+    def action_past():
+        return ungettext_lazy(
+            u"Executed test",
+            u"Executed tests"
+        )
+
+    def action(self, request, datum_id):
+        return (cloudvalidation_ostf_adapter.
+                cloudvalidation_ostf_client().tests.run(
+                datum_id, 'fuel_health'))
+
+
+class OSTFTable(tables.DataTable):
+    name = tables.Column("test", verbose_name=_("Test"))
+    report = tables.Column("report", verbose_name=_("Report"))
+
+    class Meta(object):
+        name = "OSTF tests"
+        verbose_name = _("OSTF tests")
+        # row_actions = (ExecuteTest, )
