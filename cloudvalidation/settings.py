d941cf7756234c2c3e01329b0a52eb294e28a446
Denis Makogon <dmakogon@mirantis.com>
2015-04-06 14:14:33 +0300

    Fix settings.py

--- cloudvalidation/settings.py
+++ cloudvalidation/settings.py
@@ -16,9 +16,10 @@
 #    License for the specific language governing permissions and limitations
 #    under the License.
 
+import logging
 import os
-import socket
 import sys
+import warnings
 
 from cinderclient import exceptions as cinderclient
 from glanceclient.common import exceptions as glanceclient
@@ -32,124 +33,42 @@ from swiftclient import client as swiftclient
 from troveclient import exceptions as troveclient
 
 import django
-from django.utils import html_parser
+from django.utils.translation import ugettext_lazy as _
+
 from openstack_dashboard.static_settings import get_staticfiles_dirs  # noqa
 
-from horizon.test import patches
 
-STATICFILES_DIRS = get_staticfiles_dirs()
-
-# Patch django.utils.html_parser.HTMLParser as a workaround for bug 1273943
-if django.get_version() == '1.4' and sys.version_info[:3] > (2, 7, 3):
-    html_parser.HTMLParser.parse_starttag = patches.parse_starttag_patched
-
-socket.setdefaulttimeout(1)
-
-LOGIN_URL = '/auth/login/'
-LOGOUT_URL = '/auth/logout/'
-LOGIN_REDIRECT_URL = '/'
+warnings.formatwarning = lambda message, category, *args, **kwargs: \
+    '%s: %s' % (category.__name__, message)
 
 ROOT_PATH = os.path.dirname(os.path.abspath(__file__))
-DEBUG = True
+BIN_DIR = os.path.abspath(os.path.join(ROOT_PATH, '..', 'bin'))
+
+if ROOT_PATH not in sys.path:
+    sys.path.append(ROOT_PATH)
+
+DEBUG = False
 TEMPLATE_DEBUG = DEBUG
-TESTSERVER = 'http://testserver'
 
-SECRET_KEY = 'elj1IWiLoWHgcyYxFVLj7cM5rGOOxWl0'
+SITE_BRANDING = 'OpenStack Dashboard'
 
-USE_I18N = True
-USE_L10N = True
-USE_TZ = True
+WEBROOT = '/'
+LOGIN_URL = None
+LOGOUT_URL = None
+LOGIN_REDIRECT_URL = None
 
-DATABASES = {'default': {'ENGINE': 'django.db.backends.sqlite3'}}
-
-DEFAULT_EXCEPTION_REPORTER_FILTER = 'horizon.exceptions.HorizonReporterFilter'
-
-INSTALLED_APPS = (
-    'django.contrib.sessions',
-    'django.contrib.staticfiles',
-    'django.contrib.messages',
-    'django.contrib.humanize',
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django_nose',
-    'django_pyscss',
-    'compressor',
-    'horizon',
-    'horizon.test',
-    'horizon.test.test_dashboards.cats',
-    'horizon.test.test_dashboards.dogs'
-)
-
-
-MIDDLEWARE_CLASSES = (
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-)
-if django.VERSION >= (1, 8, 0):
-    MIDDLEWARE_CLASSES += (
-        'django.contrib.auth.middleware.SessionAuthenticationMiddleware',)
-else:
-    MIDDLEWARE_CLASSES += ('django.middleware.doc.XViewMiddleware',)
-MIDDLEWARE_CLASSES += (
-    'horizon.middleware.HorizonMiddleware',
-    'django.middleware.locale.LocaleMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-)
-
-TEMPLATE_CONTEXT_PROCESSORS = (
-    'django.core.context_processors.debug',
-    'django.core.context_processors.i18n',
-    'django.core.context_processors.request',
-    'django.core.context_processors.media',
-    'django.core.context_processors.static',
-    'django.contrib.messages.context_processors.messages',
-    'horizon.context_processors.horizon')
-
-TEMPLATE_LOADERS = (
-    'django.template.loaders.filesystem.Loader',
-    'django.template.loaders.app_directories.Loader',
-    'horizon.loaders.TemplateLoader'
-)
-
-STATIC_URL = '/static/'
-
-MESSAGE_STORAGE = 'django.contrib.messages.storage.cookie.CookieStorage'
 
 ROOT_URLCONF = 'openstack_dashboard.urls'
-TEMPLATE_DIRS = (
-    os.path.join(ROOT_PATH, 'tests', 'templates'),
-    os.path.join(ROOT_PATH, '../cloudvalidation'),
-)
-SITE_ID = 1
-SITE_BRANDING = 'Horizon'
-
-TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
-NOSE_ARGS = ['--nocapture',
-             '--nologcapture',
-             '--exclude-dir=horizon/conf/',
-             '--exclude-dir=horizon/test/customization',
-             '--cover-package=horizon',
-             '--cover-inclusive',
-             '--all-modules']
-
-EMAIL_BACKEND = 'django.core.mail.backends.locmem.EmailBackend'
-SESSION_ENGINE = 'django.contrib.sessions.backends.signed_cookies'
-SESSION_COOKIE_HTTPONLY = True
-SESSION_EXPIRE_AT_BROWSER_CLOSE = True
-SESSION_COOKIE_SECURE = False
 
 HORIZON_CONFIG = {
-    'dashboards': ('cats', 'dogs'),
-    'default_dashboard': 'cats',
-    "password_validator": {
-        "regex": '^.{8,18}$',
-        "help_text": "Password must be between 8 and 18 characters."
+    'user_home': 'openstack_dashboard.views.get_user_home',
+    'ajax_queue_limit': 10,
+    'auto_fade_alerts': {
+        'delay': 3000,
+        'fade_duration': 1500,
+        'types': ['alert-success', 'alert-info']
     },
-    'user_home': None,
-    'help_url': "http://example.com",
+    'help_url': "http://docs.openstack.org",
     'exceptions': {'recoverable': (keystoneclient.ClientException,
                                    keystoneclient.AuthorizationFailure,
                                    keystoneclient.Forbidden,
@@ -180,13 +99,74 @@ HORIZON_CONFIG = {
                                     glanceclient.Unauthorized,
                                     neutronclient.Unauthorized,
                                     heatclient.HTTPUnauthorized,
-                                    troveclient.Unauthorized)}
+                                    troveclient.Unauthorized)},
+    'angular_modules': [],
+    'js_files': [],
+    'js_spec_files': [],
 }
 
-COMPRESS_ENABLED = True
-COMPRESS_OFFLINE = False
-COMPRESS_ROOT = "/tmp/"
-COMPRESS_PARSER = 'compressor.parser.HtmlParser'
+# Set to True to allow users to upload images to glance via Horizon server.
+# When enabled, a file form field will appear on the create image form.
+# See documentation for deployment considerations.
+HORIZON_IMAGES_ALLOW_UPLOAD = True
+
+# The OPENSTACK_IMAGE_BACKEND settings can be used to customize features
+# in the OpenStack Dashboard related to the Image service, such as the list
+# of supported image formats.
+OPENSTACK_IMAGE_BACKEND = {
+    'image_formats': [
+        ('', _('Select format')),
+        ('aki', _('AKI - Amazon Kernel Image')),
+        ('ami', _('AMI - Amazon Machine Image')),
+        ('ari', _('ARI - Amazon Ramdisk Image')),
+        ('iso', _('ISO - Optical Disk Image')),
+        ('ova', _('OVA - Open Virtual Appliance')),
+        ('qcow2', _('QCOW2 - QEMU Emulator')),
+        ('raw', _('Raw')),
+        ('vdi', _('VDI - Virtual Disk Image')),
+        ('vhd', _('VHD - Virtual Hard Disk')),
+        ('vmdk', _('VMDK - Virtual Machine Disk')),
+    ]
+}
+
+MIDDLEWARE_CLASSES = (
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+)
+if django.VERSION >= (1, 8, 0):
+    MIDDLEWARE_CLASSES += (
+        'django.contrib.auth.middleware.SessionAuthenticationMiddleware',)
+else:
+    MIDDLEWARE_CLASSES += ('django.middleware.doc.XViewMiddleware',)
+MIDDLEWARE_CLASSES += (
+    'horizon.middleware.HorizonMiddleware',
+    'django.middleware.locale.LocaleMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+)
+
+TEMPLATE_CONTEXT_PROCESSORS = (
+    'django.core.context_processors.debug',
+    'django.core.context_processors.i18n',
+    'django.core.context_processors.request',
+    'django.core.context_processors.media',
+    'django.core.context_processors.static',
+    'django.contrib.messages.context_processors.messages',
+    'horizon.context_processors.horizon',
+    'openstack_dashboard.context_processors.openstack',
+)
+
+TEMPLATE_LOADERS = (
+    'django.template.loaders.filesystem.Loader',
+    'django.template.loaders.app_directories.Loader',
+    'horizon.loaders.TemplateLoader',
+)
+
+TEMPLATE_DIRS = (
+    os.path.join(ROOT_PATH, 'templates'),
+)
 
 STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
@@ -194,38 +174,201 @@ STATICFILES_FINDERS = (
     'compressor.finders.CompressorFinder',
 )
 
-LOGGING = {
-    'version': 1,
-    'disable_existing_loggers': False,
-    'handlers': {
-        'null': {
-            'level': 'DEBUG',
-            'class': 'django.utils.log.NullHandler',
-        },
-        'test': {
-            'level': 'ERROR',
-            'class': 'logging.StreamHandler',
-        }
-    },
-    'loggers': {
-        'django.db.backends': {
-            'handlers': ['null'],
-            'propagate': False,
-        },
-        'horizon': {
-            'handlers': ['test'],
-            'propagate': False,
-        },
-        'nose.plugins.manager': {
-            'handlers': ['null'],
-            'propagate': False,
-        },
-        'selenium': {
-            'handlers': ['null'],
-            'propagate': False,
-        }
-    }
+COMPRESS_PRECOMPILERS = (
+    ('text/scss', 'django_pyscss.compressor.DjangoScssFilter'),
+)
+
+COMPRESS_CSS_FILTERS = (
+    'compressor.filters.css_default.CssAbsoluteFilter',
+)
+
+COMPRESS_ENABLED = True
+COMPRESS_OUTPUT_DIR = 'dashboard'
+COMPRESS_CSS_HASHING_METHOD = 'hash'
+COMPRESS_PARSER = 'compressor.parser.HtmlParser'
+
+INSTALLED_APPS = [
+    'openstack_dashboard',
+    'django.contrib.contenttypes',
+    'django.contrib.auth',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'django.contrib.humanize',
+    'django_pyscss',
+    'openstack_dashboard.django_pyscss_fix',
+    'compressor',
+    'horizon',
+    'openstack_auth',
+]
+
+TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
+AUTHENTICATION_BACKENDS = ('openstack_auth.backend.KeystoneBackend',)
+AUTHENTICATION_URLS = ['openstack_auth.urls']
+MESSAGE_STORAGE = 'django.contrib.messages.storage.fallback.FallbackStorage'
+
+SESSION_ENGINE = 'django.contrib.sessions.backends.signed_cookies'
+SESSION_COOKIE_HTTPONLY = True
+SESSION_EXPIRE_AT_BROWSER_CLOSE = True
+SESSION_COOKIE_SECURE = False
+SESSION_TIMEOUT = 1800
+# A token can be near the end of validity when a page starts loading, and
+# invalid during the rendering which can cause errors when a page load.
+# TOKEN_TIMEOUT_MARGIN defines a time in seconds we retrieve from token
+# validity to avoid this issue. You can adjust this time depending on the
+# performance of the infrastructure.
+TOKEN_TIMEOUT_MARGIN = 10
+
+# When using cookie-based sessions, log error when the session cookie exceeds
+# the following size (common browsers drop cookies above a certain size):
+SESSION_COOKIE_MAX_SIZE = 4093
+
+# when doing upgrades, it may be wise to stick to PickleSerializer
+# NOTE(berendt): Check during the K-cycle if this variable can be removed.
+#                https://bugs.launchpad.net/horizon/+bug/1349463
+SESSION_SERIALIZER = 'django.contrib.sessions.serializers.PickleSerializer'
+
+LANGUAGES = (
+    ('de', 'German'),
+    ('en', 'English'),
+    ('en-au', 'Australian English'),
+    ('en-gb', 'British English'),
+    ('es', 'Spanish'),
+    ('fr', 'French'),
+    ('hi', 'Hindi'),
+    ('ja', 'Japanese'),
+    ('ko', 'Korean (Korea)'),
+    ('nl', 'Dutch (Netherlands)'),
+    ('pl', 'Polish'),
+    ('pt-br', 'Portuguese (Brazil)'),
+    ('sr', 'Serbian'),
+    ('zh-cn', 'Simplified Chinese'),
+    ('zh-tw', 'Chinese (Taiwan)'),
+)
+LANGUAGE_CODE = 'en'
+LANGUAGE_COOKIE_NAME = 'horizon_language'
+USE_I18N = True
+USE_L10N = True
+USE_TZ = True
+
+OPENSTACK_KEYSTONE_DEFAULT_ROLE = '_member_'
+
+DEFAULT_EXCEPTION_REPORTER_FILTER = 'horizon.exceptions.HorizonReporterFilter'
+
+POLICY_FILES_PATH = os.path.join(ROOT_PATH, "conf")
+# Map of local copy of service policy files
+POLICY_FILES = {
+    'identity': 'keystone_policy.json',
+    'compute': 'nova_policy.json',
+    'volume': 'cinder_policy.json',
+    'image': 'glance_policy.json',
+    'orchestration': 'heat_policy.json',
+    'network': 'neutron_policy.json',
+    'telemetry': 'ceilometer_policy.json',
 }
+
+SECRET_KEY = None
+LOCAL_PATH = None
+
+SECURITY_GROUP_RULES = {
+    'all_tcp': {
+        'name': _('All TCP'),
+        'ip_protocol': 'tcp',
+        'from_port': '1',
+        'to_port': '65535',
+    },
+    'all_udp': {
+        'name': _('All UDP'),
+        'ip_protocol': 'udp',
+        'from_port': '1',
+        'to_port': '65535',
+    },
+    'all_icmp': {
+        'name': _('All ICMP'),
+        'ip_protocol': 'icmp',
+        'from_port': '-1',
+        'to_port': '-1',
+    },
+}
+
+ADD_INSTALLED_APPS = []
+
+# STATIC directory for custom theme, set as default.
+# It can be overridden in local_settings.py
+CUSTOM_THEME_PATH = 'static/themes/default'
+
+try:
+    from local.local_settings import *  # noqa
+except ImportError:
+    logging.warning("No local_settings file found.")
+
+if not WEBROOT.endswith('/'):
+    WEBROOT += '/'
+if LOGIN_URL is None:
+    LOGIN_URL = WEBROOT + 'auth/login/'
+if LOGOUT_URL is None:
+    LOGOUT_URL = WEBROOT + 'auth/logout/'
+if LOGIN_REDIRECT_URL is None:
+    LOGIN_REDIRECT_URL = WEBROOT
+
+MEDIA_ROOT = os.path.abspath(os.path.join(ROOT_PATH, '..', 'media'))
+MEDIA_URL = WEBROOT + 'media/'
+STATIC_ROOT = os.path.abspath(os.path.join(ROOT_PATH, '..', 'static'))
+STATIC_URL = WEBROOT + 'static/'
+STATICFILES_DIRS = get_staticfiles_dirs(WEBROOT)
+
+CUSTOM_THEME = os.path.join(ROOT_PATH, CUSTOM_THEME_PATH)
+STATICFILES_DIRS.append(
+    ('custom', CUSTOM_THEME),
+)
+
+# Load the pluggable dashboard settings
+import openstack_dashboard.enabled
+import openstack_dashboard.local.enabled
+from openstack_dashboard.utils import settings
+
+INSTALLED_APPS = list(INSTALLED_APPS)  # Make sure it's mutable
+settings.update_dashboards(
+    [
+        openstack_dashboard.enabled,
+        openstack_dashboard.local.enabled,
+    ],
+    HORIZON_CONFIG,
+    INSTALLED_APPS,
+)
+INSTALLED_APPS[0:0] = ADD_INSTALLED_APPS
+
+# Ensure that we always have a SECRET_KEY set, even when no local_settings.py
+# file is present. See local_settings.py.example for full documentation on the
+# horizon.utils.secret_key module and its use.
+if not SECRET_KEY:
+    if not LOCAL_PATH:
+        LOCAL_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)),
+                                  'local')
+
+    from horizon.utils import secret_key
+    SECRET_KEY = secret_key.generate_or_read_from_file(os.path.join(LOCAL_PATH,
+                                                       '.secret_key_store'))
+
+from openstack_dashboard import policy_backend
+POLICY_CHECK_FUNCTION = policy_backend.check
+
+# Add HORIZON_CONFIG to the context information for offline compression
+COMPRESS_OFFLINE_CONTEXT = {
+    'STATIC_URL': STATIC_URL,
+    'HORIZON_CONFIG': HORIZON_CONFIG,
+}
+
+if DEBUG:
+    logging.basicConfig(level=logging.DEBUG)
+
+# during django reloads and an active user is logged in, the monkey
+# patch below will not otherwise be applied in time - resulting in developers
+# appearing to be logged out.  In typical production deployments this section
+# below may be omitted, though it should not be harmful
+from openstack_auth import utils as auth_utils
+auth_utils.patch_middleware_get_user()
+
 
 OPENSTACK_KEYSTONE_URL = os.environ.get("OS_AUTH_URL", "http://172.18.196.219:5000/v2.0/")
 
@@ -243,3 +386,9 @@ settings.update_dashboards(
     INSTALLED_APPS,
 )
 INSTALLED_APPS[0:0] = ADD_INSTALLED_APPS
+
+TEMPLATE_DIRS = list(TEMPLATE_DIRS)
+TEMPLATE_DIRS.append(
+    os.path.join(ROOT_PATH, '../cloudvalidation')
+)
+TEMPLATE_DIRS = tuple(TEMPLATE_DIRS)
